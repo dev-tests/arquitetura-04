fe3c32b2ca6356383aec65d978939f42a11dddea
ronielramos <roniel_ramos@hotmail.com>
2020-01-06 20:27:02 -0300

    renomear model

--- src/models/investidor.model.ts
+++ src/models/investidor.model.ts
@@ -1,9 +1,9 @@
 // Modelo de Carro igual ao Banco de dados
 // Cada modelo deve ter seu próprio arquivo
 
-export interface CarroModel {
-    idCarro: number; // int, decimal
-    nomeCarro: string; // Varchar, char
+export interface InvestidorModel {
+    idInvestidor: number;
+    nomeInvestidor: string;
     created: Date;
     updated: Date;
 }
